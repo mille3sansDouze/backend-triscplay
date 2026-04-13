ec4aa8588d2bc9234ddcd417dc59b3fffa5375cf
Guillaume <sorgues29@gmail.com>
2026-04-13 10:04:18 +0200

    added controllers on Game, added a update-game-dto for a better method, modified @patch and service in consequence

--- src/domains/game/game.controller.ts
+++ src/domains/game/game.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, UseGuards } from '@nestjs/common';
 import { GameInterface, GameService } from './game.service';
 import { CreateGameDto } from './dto/create-game.dto';
+import { SessionGuard } from 'src/common/guards/session.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UpdateGameDto } from './dto/update-game.dto';
 
 @Controller('game')
 export class GameController {
     constructor(private readonly gameService: GameService) {}
 
+  //ROUTES PUBLIQUES
   @Get()
   findAll(): Promise<GameInterface[]> {
     return this.gameService.findAll();
@@ -17,18 +22,25 @@ export class GameController {
     return this.gameService.findOne(+id);
   }
 
+  //ROUTES ADMIN UNIQUEMENT
   @Post()
+  @UseGuards(SessionGuard, RolesGuard)
+  @Roles('admin')
   create(@Body() body:CreateGameDto){
     return this.gameService.create(body);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() game: Partial<GameInterface>): Promise<GameInterface | null> {
+  @UseGuards(SessionGuard, RolesGuard)
+  @Roles('admin')
+  update(@Param('id') id: string, @Body() body: UpdateGameDto ){
 
-    return this.gameService.update(+id, game);
+    return this.gameService.update(+id, body);
   }
 
   @Delete(':id')
+  @UseGuards(SessionGuard, RolesGuard)
+  @Roles('admin')
   remove(@Param('id') id: string): Promise<void> {
     return this.gameService.remove(+id);
   }
