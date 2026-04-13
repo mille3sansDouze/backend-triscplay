9634ba41ceea1964f1be2f68740a1e528e651f15
Guillaume <sorgues29@gmail.com>
2026-04-13 14:36:16 +0200

    added Swagger in the packages, initialized it in the main.ts, and create-game.dto

--- src/domains/game/dto/create-game.dto.ts
+++ src/domains/game/dto/create-game.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, IsOptional, IsNumber, IsBoolean} from 'class-validator';
 export class CreateGameDto {
+    @ApiProperty({ example: 'Minecraft', description: 'Nom du jeu'})
     @IsString()
     @IsNotEmpty({message:"Vous devez entrer un nom de jeu"})
     name: string;
 
+    @ApiProperty({ example: 'Ceci est un jeu de bac à sable', description:'Description du jeu'})
     @IsString()
     @MaxLength(300)
     @IsOptional()
     description: string;
 
+    @ApiProperty({ example: '7', description: 'Le PEGI du jeu'})
     @IsNumber()
     @IsOptional()
     pegi: number;
 
+    @ApiProperty({ example: '4', description: 'Le nombre de joueur qui peuvent y jouer'})
     @IsNumber()
     @IsNotEmpty({message:"Rentrez un nombre de joueurs (min1)"})
     player_count: number;
 
+    @ApiProperty({ example: 'true', description: 'Existe-t-il un mode multijouer au jeu ?'})
     @IsBoolean()
     @IsNotEmpty({message:"Renseignez si le jeu est multijouer ou non"})
     is_multiplayer: boolean;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('TriscPlay API')
+    .setDescription("Documentation de l'API TriscPlay")
+    .setVersion('1.0')
+    .addApiKey(
+      { type: 'apiKey', in: 'header', name: 'x-session-id' },
+      'session-id',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
 
   await app.listen(process.env.PORT ?? 3000);
 }
