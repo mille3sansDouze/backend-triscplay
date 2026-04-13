90d06daa8191a277f6f907afdfd13c046ef27370
Guillaume <sorgues29@gmail.com>
2026-04-13 13:52:19 +0200

    fix: added @IsOptional decorators on update-user-dto

--- src/domains/user/dto/update-user.dto.ts
+++ src/domains/user/dto/update-user.dto.ts
@@ -1,15 +1,19 @@
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
+    @IsOptional()
     @IsString()
     user_name?: string;
 
+    @IsOptional()
     @IsString()
     password?: string;
 
+    @IsOptional()
     @IsString()
     profile_pic_url?: string;
 
+    @IsOptional()
     @IsString()
     description?: string;
 }
