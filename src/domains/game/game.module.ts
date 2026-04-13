77a498478146e51b364fccecb7409d3da1a45aa8
Guillaume <sorgues29@gmail.com>
2026-04-13 10:57:59 +0200

    fix: added SessionModule in game.module and session.module so the server can start

--- src/domains/game/game.module.ts
+++ src/domains/game/game.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './game.entity';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { SessionModule } from 'src/technical/session/session.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Game])],
+  imports: [TypeOrmModule.forFeature([Game]), SessionModule],
   controllers: [GameController],
   providers: [GameService],
 })
--- src/domains/scoreboard/scoreboard.module.ts
+++ src/domains/scoreboard/scoreboard.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Scoreboard } from './scoreboard.entity';
 import { ScoreboardService } from './scoreboard.service';
 import { ScoreboardController } from './scoreboard.controller';
+import { SessionModule } from 'src/technical/session/session.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Scoreboard])],
+  imports: [TypeOrmModule.forFeature([Scoreboard]), SessionModule],
   providers: [ScoreboardService],
   controllers: [ScoreboardController]
 })
