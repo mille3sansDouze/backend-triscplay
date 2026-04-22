81bf8a5d1b8d30c43b7ce2f1a699972c4aef8567
Guillaume <sorgues29@gmail.com>
2026-04-22 16:13:18 +0200

    added IsOptional on update-game.dto

--- src/domains/game/dto/update-game.dto.ts
+++ src/domains/game/dto/update-game.dto.ts
@@ -1,28 +1,34 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsBoolean, IsNumber, IsString } from "class-validator";
+import { IsBoolean, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class UpdateGameDto {
     @ApiProperty({example: 'NewGameName', description:'Le nouveau nom du jeu'})
     @IsString()
+    @IsOptional()
     name?: string;
 
     @ApiProperty({example: 'NewDescription', description:'La nouvelle description'})
     @IsString()
+    @IsOptional()
     description?: string;
 
     @ApiProperty({example: 16, description:'Le PEGI était mal entré'})
     @IsNumber()
+    @IsOptional()
     pegi?: number;
 
     @ApiProperty({example: 8, description:'Le nombre de joueur a changé'})
     @IsNumber()
+    @IsOptional()
     player_count?: number;
 
     @ApiProperty({example: false, description:"Le multiplayer est supporté/n'est plus supporté"})
     @IsBoolean()
+    @IsOptional()
     is_multiplayer?: boolean;
         
     @ApiProperty({example: 'NewUrl', description:'La nouvelle url image'})
     @IsString()
+    @IsOptional()
     img_url?: string;
 }
